a5a119e37a4ab14e0d12529ffb6824dc0ab9accf
Christian Gonzalez <chrisalmonte@protonmail.com>
2025-03-29 15:09:37 -0600

    Add stopword detection

--- Similitud_Palabras.py
+++ Similitud_Palabras.py
@@ -3,10 +3,10 @@ import nltk
 import os
 import re
 from bs4 import BeautifulSoup
-#nltk.download('punkt_tab')
 
 #Config.
-corpus = "../Corpora/e990505_mod_lemmatized_spacy.txt"
+corpus = "../../Corpora/e990505_mod_lemmatized_spacy.txt"
+stopwords_list = nltk.corpus.stopwords.words("spanish")
 sentence_tokenizer = nltk.data.load('tokenizers/punkt_tab/spanish.pickle')
 word_tokenizer = nltk.tokenize.ToktokTokenizer()
 
@@ -20,14 +20,18 @@ def str_from_file(file_path: str, file_encoding: str="utf-8", parse_HTML=True):
         return soup.get_text()
     return raw
 
-#Función para generar tokens únicos
-def gen_vocab(text: str):
+#Función para generar tokens únicos y enunciados procesados.
+def normalize_text(text: str, remove_stopwords=False, stopwords: list=None):
+    if remove_stopwords and stopwords is None:
+        raise ValueError("No se proporcionó lista de stopwords.")
     #Tokenizar oraciones.
     sentence_tokens = sentence_tokenizer.tokenize(text)
-    sentence_tokens = list(set(sentence_tokens))
+    sentence_tokens = list(set(sentence_tokens))    
     #Tokenizar palabras
     word_tokens = set()
+    sentences_processed = []
     for sentence in sentence_tokens:
+        processed_sentence = []
         tokens = word_tokenizer.tokenize(sentence)
         #Filtrar tokens.
         for token in tokens:
@@ -45,8 +49,13 @@ def gen_vocab(text: str):
             #Agregar en minúsculas.
             token = token.lower()
             token = re.sub(r"[\W\-]", "", token)
+            #Ignorar stopwords
+            if remove_stopwords and (token in stopwords):
+                continue
+            processed_sentence.append(token)
             word_tokens.add(token)
-    return sorted(list(word_tokens))
+        sentences_processed.append(processed_sentence)
+    return (sorted(word_tokens), sentences_processed)
 
 #Función para guardar la salida en un archivo
 def save(content, filename: str, description: str = ""):
@@ -60,16 +69,13 @@ def save(content, filename: str, description: str = ""):
     with open(path, 'w', encoding='utf-8') as file:
         file.write("%s\n\n" % description)
         file.write(content)
-        #for obj in content:
-        #    file.write(str(obj) + "\n")
         file.write("\n")
 
 #Generar vectores de palabras
 text = str_from_file(corpus, parse_HTML=False)
-#save(text, "texto_extraido.txt", "Texto sin etiquetas extraído del archivo \"%s\":" % corpus) 
-vocab = gen_vocab(text)
-save("\n".join(vocab), "vocabulario.txt", "Vocabulario del corpus:")
+#save(text, "texto_extraido.txt", "Texto sin etiquetas extraído del archivo \"%s\":" % corpus)
+doc = normalize_text(text, remove_stopwords=True, stopwords=stopwords_list)
+save("\n".join(sorted(stopwords_list)), "stopwords.txt", "Lista de stopwords usadas (NLTK Corpus):")
+save("\n".join(doc[0]), "vocabulario.txt", "Vocabulario del corpus:")
+save("\n\n".join(" ".join(sentence) for sentence in doc[1]), "oraciones.txt", "Oraciones normalizadas del corpus:")
 
-#Recopilar Vocabulario
-#Tokenizar (Existen varios tokenizers, incluso dentro de nltk)
-#words = nltk.word_tokenize(text)
